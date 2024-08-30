5385278e917d8d95a3396993d022d2875d87c030
kjm541006 <kjm541113@gmail.com>
2024-08-30 16:48:07 +0900

    404 에러 처리 페이지 구현

--- springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
+++ springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
@@ -1,9 +1,12 @@
 package org.zerock.springex.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Arrays;
 
@@ -39,5 +42,12 @@ public class CommonExceptionAdvice {
         buffer.append("</ul>");
 
         return buffer.toString();
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound(){
+
+        return "custom404";
     }
 }
