c21317e9a534605597456ad1a3f02517e3d9fa76
kjm541006 <kjm541113@gmail.com>
2024-09-02 15:57:08 +0900

    Todo Register 한글 문제 해결 및 서버사이드 검증 추가

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,12 +2,15 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/todo")
@@ -26,8 +29,15 @@ public class TodoController {
     }
 
     @PostMapping("/register")
-    public String registerPOST(TodoDTO todoDTO, RedirectAttributes redirectAttributes){
+    public String registerPOST(@Valid TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
         log.info("POST todo register........");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors.........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/todo/register";
+        }
+
         log.info(todoDTO);
 
         return "redirect:/todo/list";
