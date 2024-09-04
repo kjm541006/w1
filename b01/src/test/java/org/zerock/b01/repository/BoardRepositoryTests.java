38990eefb2d36e1df116d75d9c88c0a0196b8e41
kjm541006 <kjm541113@gmail.com>
2024-09-04 22:58:42 +0900

    Pageable 과 Page<E> 타입 실습

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -61,5 +66,22 @@ public class BoardRepositoryTests {
         Long bno = 1L;
 
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging(){
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count: " + result.getTotalElements());
+        log.info("total pages: " + result.getTotalPages());
+        log.info("page number: " + result.getNumber());
+        log.info("page size: "+ result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
     }
 }
