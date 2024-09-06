a9210f2db23bce61d8c45c9f98578ef749075383
kjm541006 <kjm541113@gmail.com>
2024-09-06 18:09:56 +0900

    등록 처리 테스트

---
+++ b01/src/main/java/org/zerock/b01/dto/BoardDTO.java
@@ -0,0 +1,28 @@
+package org.zerock.b01.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDTO {
+
+    private Long tno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime modDate;
+
+}
