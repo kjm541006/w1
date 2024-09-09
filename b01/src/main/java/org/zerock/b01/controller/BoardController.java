44b3bf46ec97d1a0281355fd0968f98e07640f94
kjm541006 <kjm541113@gmail.com>
2024-09-09 18:44:19 +0900

    수정처리 및 화면 구현

--- b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -60,7 +60,7 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
 
         BoardDTO boardDTO = boardService.readOne(bno);
@@ -68,6 +68,33 @@ public class BoardController {
         log.info("boardDTO");
 
         model.addAttribute("dto", boardDTO);
+    }
+
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO, @Valid BoardDTO boardDTO,
+                         BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        log.info("board modify post......." + boardDTO);
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors.....");
+
+            String link = pageRequestDTO.getLink();
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+            return "redirect:/board/modify";
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modified");
+
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+        return "redirect:/board/read";
     }
 }
 
