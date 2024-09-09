cacc3d5a057750de513f8aff1aef5377c0c6ffb2
kjm541006 <kjm541113@gmail.com>
2024-09-09 17:32:04 +0900

    register 구현

--- b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,11 +1,15 @@
 package org.zerock.b01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
@@ -29,4 +33,54 @@ public class BoardController {
 
         model.addAttribute("responseDTO", responseDTO);
     }
+
+    @GetMapping("/register")
+    public void getRegister(){
+
+    }
+
+    @PostMapping("/register")
+    public String postRegister(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        log.info("board POST register......");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors.....");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
