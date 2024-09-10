0c7b98a9585e625418ec023f0c83e272835f57d7
kjm541006 <kjm541113@gmail.com>
2024-09-10 19:04:04 +0900

    댓글 조회 테스트(단방향)

--- b01/src/test/java/org/zerock/b01/repository/ReplyRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/ReplyRepositoryTests.java
@@ -5,6 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 import org.zerock.b01.domain.Reply;
 
@@ -29,6 +33,20 @@ public class ReplyRepositoryTests {
                 .build();
 
         replyRepository.save(reply);
+    }
+
+    @Test
+    public void testBoardReplies(){
+
+        Long bno = 100L;
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("rno").descending());
+
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> {
+            log.info(reply);
+        });
     }
 
 }
