c80d31a732efb3145d7baf466091230061c4d713
kjm541006 <kjm541113@gmail.com>
2024-09-14 23:55:33 +0900

    특정 게시물의 댓글 목록

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -8,10 +8,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 import org.zerock.b01.service.ReplyService;
 
@@ -44,4 +43,12 @@ public class ReplyController {
 
         return resultMap;
     }
+//
+//    @Operation(summary = "Replies of Board", description = "GET 방식으로 특정 게시물의 댓글 목록")
+//    @GetMapping(value = "/list/{bno}")
+//    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO){
+//        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+//
+//        return responseDTO;
+//    }
 }
