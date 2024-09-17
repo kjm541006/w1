a11b399510f7f6cdcf0dbcf4161df57e7d4c390b
kjm541006 <kjm541113@gmail.com>
2024-09-17 17:01:20 +0900

    댓글 수정 및 예외 처리

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -73,4 +73,19 @@ public class ReplyController {
 
         return resultMap;
     }
+
+    @Operation(summary = "Modify Reply", description = "PUT 방식으로 특정 댓글 수정")
+    @PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> modify(@PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO){
+
+        replyDTO.setRno(rno);
+
+        replyService.modify(replyDTO);
+
+        Map<String, Long> resultMap = new HashMap<>();
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
+    }
 }
