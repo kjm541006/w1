cb430c41fce68cd949b9fbb35f40ebd00709c29b
kjm541006 <kjm541113@gmail.com>
2024-09-27 16:29:29 +0900

    RedirectAttributes 적용

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -90,11 +91,23 @@ public class BasicItemController {
     }
 
     // PRG 패턴 적용
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV5(Item item){
         itemRepository.save(item);
 
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        // itemId는 url에 path variable로 들어가서 그대로 사용되고
+        // status는 url에 없으므로 쿼리 파라미터로 들어감
+        return "redirect:/basic/items/{itemId}";
+        // uri => /basic/items/{itemId}?status=true
     }
 
     @GetMapping("/{itemId}/edit")
