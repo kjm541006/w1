77225c50909c64f8d7f124c795c7c4a0d77ab306
kjm541006 <kjm541113@gmail.com>
2024-09-29 18:34:30 +0900

    item 찾기

--- project1/src/main/java/study/project1/controller/ItemController.java
+++ project1/src/main/java/study/project1/controller/ItemController.java
@@ -1,11 +1,42 @@
 package study.project1.controller;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import study.project1.domain.Item;
+import study.project1.repository.ItemRepositoryImpl;
 
 @Controller
 @Slf4j
+@RequestMapping("/items")
 public class ItemController {
 
+    ItemRepositoryImpl itemRepository = new ItemRepositoryImpl();
+
+    @GetMapping
+    public String list(){
+
+        return "/items/list";
+    }
+
+    @GetMapping("/{itemId}")
+    public String findOne(@PathVariable Long itemId, Model model){
+        Item findItem = itemRepository.find(itemId);
+
+        model.addAttribute("item", findItem);
+
+        return "/items/item";
+    }
+
+
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
 
 }
