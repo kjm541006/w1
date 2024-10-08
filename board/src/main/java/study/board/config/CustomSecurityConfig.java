f4b7a961245795218972d41de3c3c8705ed7f6d6
kjm541006 <kjm541113@gmail.com>
2024-10-08 15:35:44 +0900

    시큐리티 설정

---
+++ board/src/main/java/study/board/config/CustomSecurityConfig.java
@@ -0,0 +1,41 @@
+package study.board.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        log.info("--------configure----------");
+
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        log.info("-----------web configure----------");
+
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest
+                        .toStaticResources().atCommonLocations());
+    }
+
+}
---
+++ board/src/main/java/study/board/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package study.board.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("loadUserByUsername: " + username);
+
+        UserDetails userDetails = User.builder()
+                .username("user1")
+                .password("1111")
+                .authorities("ROLE_USER")
+                .build();
+
+        return userDetails;
+    }
+}
