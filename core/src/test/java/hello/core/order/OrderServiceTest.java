1daed3084bfe2c70b13b96fdcfe5f4302c93a8fc
kjm541006 <kjm541113@gmail.com>
2024-11-28 22:15:41 +0900

    AppConfig 리팩터링

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -5,14 +5,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    AppConfig appConfig = new AppConfig();
+    MemberService memberService;
+    OrderService orderService;
 
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
