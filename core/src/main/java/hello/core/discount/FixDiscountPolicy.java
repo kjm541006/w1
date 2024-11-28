7a940d03ee2c84a440dd00d399a45b89c035621b
kjm541006 <kjm541113@gmail.com>
2024-11-28 16:12:02 +0900

    주문 할인 도메인 개발

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixMount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return discountFixMount;
+        }else{
+            return 0;
+        }
+    }
+}
